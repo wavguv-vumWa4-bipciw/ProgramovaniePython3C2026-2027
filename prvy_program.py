eb2f5584c0a31db12b323f03f0c824e0f67c3bb7
Mojmír Vrana <mojmir.vrana@ss-nizna.sk>
2026-09-16 13:25:45 +0200

    test2

---
+++ prvy_program.py
@@ -0,0 +1,15 @@
+print('Toto je môj prvý text')
+print("Toto je môj druhý text")
+
+x = 2
+y = 2.5
+
+premmena_x = 2
+premmenaY = 2.5
+sumaPremmenych = premmenaY + premmenaY
+
+hodnotaCiselna = input("Zadaj cele cislo")
+meno= input("Zadaj meno")
+
+
+
